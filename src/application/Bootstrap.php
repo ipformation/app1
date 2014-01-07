fc27770c73e8d43605ca7ef33176fda533b6c86d
tlm <kevinguicherd@gmail.com>
2014-01-07 17:35:16 +0100

    Structure ZF1

---
+++ src/application/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Bootstrap principal de l'application
+ * 
+ * @category    App1
+ * @desc        Bootstrap principal de l'application
+ * @author      tarik <malouli.tarik@gmail.com>
+ *  
+ */
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    
+}
---
+++ src/application/Core/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * Bootstrap du module Core
+ * 
+ * @category    App1
+ * @package     Core
+ * @desc        Bootstrap du module Core
+ * @author      tarik <malouli.tarik@gmail.com>
+ *  
+ */
+class Core_Bootstrap extends Zend_Application_Module_Bootstrap
+{
+    
+}
